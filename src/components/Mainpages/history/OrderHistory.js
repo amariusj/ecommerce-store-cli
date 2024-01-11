c0ab8954761c51294374002850e512378cd0fa42
Amarius Jones <amariusj@gmail.com>
2024-01-10 19:33:24 -0500

    Updating OrderHistory Page

--- src/components/Mainpages/history/OrderHistory.js
+++ src/components/Mainpages/history/OrderHistory.js
@@ -35,6 +35,11 @@ export default function OrderHistory() {
             }
 
             getHistory()
+
+        } else {
+
+            window.location.href = "/"
+
         }
     }, [token, isAdmin, setHistory])
 
